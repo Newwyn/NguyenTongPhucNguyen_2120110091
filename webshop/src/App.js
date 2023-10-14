25d98c460bc0698657600fb00d42b52a6bc7936c
nguyentrungphap <phapnguyen05032002@gmail.com>
2023-10-15 06:40:49 +0700

    done fe

--- webshop/src/App.js
+++ webshop/src/App.js
@@ -1,11 +1,17 @@
-import Shop from './shop/index'
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./app/layout/footer";
+import Header from "./app/layout/header";
+import Main from "./app/layout/main";
+
 
 function App() {
   return (
-    <div>
-      <Shop />
-    </div>
+    <BrowserRouter>
+  <Header />
+  <Main />
+  <Footer />
+  </BrowserRouter>
+
   );
 }
 
